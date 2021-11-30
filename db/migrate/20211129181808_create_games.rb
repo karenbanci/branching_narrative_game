26b947338b0a18bb4dc5810cc7507803a38f9ffc
Eduardo Cavalcante <ducu_yusuke@hotmail.com>
2021-11-30 14:55:14 -0300

    removed games table from migration and fixed associations

--- db/migrate/20211129181808_create_games.rb
+++
@@ -1,9 +0,0 @@
-class CreateGames < ActiveRecord::Migration[6.1]
-  def change
-    create_table :games do |t|
-      t.string :name, null: false, default: ""
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20211129182819_create_scenes.rb
+++ db/migrate/20211129182819_create_scenes.rb
@@ -1,7 +1,7 @@
 class CreateScenes < ActiveRecord::Migration[6.1]
   def change
     create_table :scenes do |t|
-      t.references :game, null: false, foreign_key: true
+      t.references :narrative, null: false, foreign_key: true
       t.text :story
 
       t.timestamps
