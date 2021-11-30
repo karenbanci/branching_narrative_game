190edcd9257bf4362da376a6219294f3036d5a15
brunoneumann <neumann.bruno90@gmail.com>
2021-11-30 15:55:42 -0300

    dev profile banner done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/devs", to: "pages#devs", as: :devs
   resource :users
   
 end
