97c5535211049f708f35d91b5a5f8f38dbb2e757
Eduardo Cavalcante <ducu_yusuke@hotmail.com>
2021-12-03 13:57:09 -0300

    scene_1 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# =============================================================================================================
+# USERS
+# =============================================================================================================
+user = User.create!(email: "test@test.com", password: "batata", username: "test")
+
+# =============================================================================================================
+# NARRATIVES
+# =============================================================================================================
+narrative = Narrative.create!(user: user)
+
+# =============================================================================================================
+# NPCS
+# =============================================================================================================
+halia = Npc.create!(name:"Halia")
+ziraldo = Npc.create!(name:"Ziraldo")
+
+# =============================================================================================================
+# SCENES
+# =============================================================================================================
+scene_1 = Scene.create!(
+  npc_position1: "left",
+  npc_position2: "right",
+  story:
+        %(Welcome, {pc_name}, to a magical land populated by men, elves, dwarves, and many other types of creatures. You are a lesser human noble, the captain of a [Confraternity] called 'The Dead Men'. You and your companions are on a months long journey on the trail of [Ardrath's Hoard]. You have set up camp in the woods and gathered around the campfire to unwind from a hard day of travel... {newline}
+        {npc1} "Night is upon us, he should be back by now. Has Jorek not returned?" {newline}
+        {npc2} "Hush, woman. I'm trying to finish my drink. If you are so concerned about the dwarf, go after him yourself." {newline}
+        {npc1} "I dare you to call me woman again you drunkard. Perhaps I should carve my name on your forehead so you don't forget it?" {newline}
+        {npc2} "Hah! I'd like to see you try. I doubt you can use those knives of yours in a fight, instead of sneaking up on your foe. Why didn't you go scouting tonight, anyways? Scared of the [Elgenwode], are we?" {newline}
+        {npc1} "I refuse to argue with a vagabond. What do you think, captain?" {newline})
+)
+
+# =============================================================================================================
+# CONSEQUENCES
+# =============================================================================================================
+consequence_1 = Consequence.create!(option: "I think you should stop worrying. Jorek can take care of himself.", result:{})
+consequence_2 = Consequence.create!(option: "I think the two of you deserve each other.", result:{})
+consequence_3 = Consequence.create!(option: "What do I think? I act. Let us search for him.", result:{}, scene: scene_2)
+
+
+# =============================================================================================================
+# CHOICES
+# =============================================================================================================
+Choice.create!(scene: scene_1, consequence: consequence_1)
+Choice.create!(scene: scene_1, consequence: consequence_2)
+Choice.create!(scene: scene_1, consequence: consequence_3)
+
+# =============================================================================================================
+# ENCOUNTER
+# =============================================================================================================
+Encounter.create!(scene: scene_1, npc: npc1)
+Encounter.create!(scene: scene_1, npc: npc2)
