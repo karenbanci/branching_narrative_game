12142359d65fa6f64caf534ee431de76be134e56
Bernardo Lima <bernardomonclaro@gmail.com>
2021-12-07 13:30:14 -0300

    ajeitei o javascript de passar a cena

--- app/javascript/components/init_narrative.js
+++ app/javascript/components/init_narrative.js
@@ -3,17 +3,18 @@ const initNarrative = () => {
   if (storyElement) {
     const story = JSON.parse(storyElement.dataset.story);
     const actionsElement = document.getElementById('actions');
-    let storyIndex = 0
+    let storyIndex = 1
     const storyLines = document.querySelectorAll('.story-line');
-    storyLines[storyIndex].classList.remove('d-none')
+
 
     document.addEventListener("keyup", (event) => {
-      if (event.code === "Enter" && storyIndex < story.length){
+      if ((event.code === "Enter" || event.code === "Space") && storyIndex <= story.length){
         storyLines.forEach((storyLine) => {
           storyLine.classList.add("d-none");
         })
-        storyLines[storyIndex].classList.remove("d-none");
-        if (storyIndex === story.length - 1){
+        storyLines[storyIndex]?.classList.remove("d-none");
+        if (storyIndex === story.length){
+          storyLines[storyLines.length - 1].classList.remove("d-none");
           actionsElement.classList.remove("d-none");
         }
         storyIndex++
