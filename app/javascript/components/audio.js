c6c71a51edf70a9eb341f469e9bcf7624bbcf41b
Bernardo Lima <bernardomonclaro@gmail.com>
2021-12-08 14:15:45 -0300

    audiorodando

--- app/javascript/components/audio.js
+++ app/javascript/components/audio.js
@@ -1,13 +1,40 @@
+var play_pause_status = "play";
+
 const backgroundAudio = () => {
   const music = document.getElementById('music');
-  const startMusic = () => {
-    music.volume = 0.5;
-    music.play();
-  }
-  const pauseMusic = () => {
-    music.pause();
-  }
-  startMusic();
-  music.addEventListener("ended", startMusic);
-}
+  if (music) {
+
+    const startMusic = () => {
+      music.volume = 0.5;
+      music.play();
+    };
+
+    const pauseMusic = () => {
+      music.volume = 0.0;
+      music.pause();
+    };
+
+    startMusic();
+    pauseMusic();
+
+    music.addEventListener("ended", startMusic);
+  };
+};
+// const backgroundAudio = () => {
+//   var play_pause_status = "play";
+
+//   const music = document.getElementById('music');
+//   const startMusic = () => {
+//     music.volume = 0.5;
+//     music.play();
+//   }
+
+//   const pauseMusic = () => {
+//     music.pause();
+//   }
+
+//   pauseMusic();
+//   startMusic();
+//   music.addEventListener("ended", startMusic);
+// }
 export{backgroundAudio}
