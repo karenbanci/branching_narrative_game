6d4782c69c63454511e84c22f5b600f758681bf4
brunoneumann <neumann.bruno90@gmail.com>
2021-12-08 17:32:23 -0300

    arrumei a nav-bar(login) + arrumado o username(profile)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permited_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || index_path
+  end
+
+  protected
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
